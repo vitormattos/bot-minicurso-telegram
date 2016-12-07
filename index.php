3158417bcd79d5d33eb7dcfe3d0dd8d91a717094
Vitor Mattos <vitor.mattos@phprio.org>
2016-12-07 15:40:56 -0200

    Implementação de mock e log de requisição

--- index.php
+++ index.php
@@ -6,6 +6,13 @@ use Telegram\Bot\Api;
 if(file_exists('.env')) {
     $dotenv = new Dotenv\Dotenv(__DIR__);
     $dotenv->load();
+    class mockApi extends Api{
+        public function getWebhookUpdates($emitUpdateWasReceivedEvent = true) {
+            return new Update(json_decode(getenv('MOCK_JSON'), true));
+        }
+    }
+    $telegram = new mockApi($config['token']);
+} else {
+    error_log(file_get_contents('php://input'));
+    $telegram = new Api();
 }
-
-$telegram = new Api();
