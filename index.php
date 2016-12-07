58b5760a42f93c0bb073171574182b964145a154
Vitor Mattos <vitor.mattos@phprio.org>
2016-12-07 16:27:05 -0200

    Mostra CEP

--- index.php
+++ index.php
@@ -34,5 +34,36 @@ if($update->has('message')) {
             case (preg_match('/^\/start (?<token>[a-f0-9]{32})/', $text, $matches) ? true : false):
                 break;
         }
+    } elseif($message->has('location')) {
+        $location = $message->getLocation();
+        $url = 'http://maps.googleapis.com/maps/api/geocode/json?'.
+            'latlng='.$location->getLatitude().','.$location->getLongitude().'&sensor=false&language=pt';
+        if($response = \file_get_contents($url)) {
+            $response = json_decode($response, true);
+            if($response['status'] == 'OK') {
+                if (isset($response['results'][0]['address_components'])) {
+                    $cep = array_filter(
+                        $response['results'][0]['address_components'],
+                        function($var) {
+                            return in_array('postal_code', $var['types']);
+                        }
+                    );
+                    if($cep) {
+                        $cep = current($cep);
+                        $cep = $cep['long_name'];
+                        $telegram->sendMessage([
+                            'chat_id' => $message->getChat()->getId(),
+                            'text' => 'Seu CEP é: '. $cep,
+                        ]);
+                    }
+                }
+            }
+        }
+        if(!$cep) {
+            $telegram->sendMessage([
+                'chat_id' => $message->getChat()->getId(),
+                'text' => 'Não descobri seu CEP :cry:',
+            ]);
+        }
     }
 }
