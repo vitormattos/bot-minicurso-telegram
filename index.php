505e5372dbf2c658577111add0ffd6395523e0aa
Vitor Mattos <vitor.mattos@phprio.org>
2016-12-07 15:55:43 -0200

    Recebimento de comandos e resposta aos comandos enviados

--- index.php
+++ index.php
@@ -16,3 +16,21 @@ if(file_exists('.env')) {
     error_log(file_get_contents('php://input'));
     $telegram = new Api();
 }
+
+$update = $telegram->getWebhookUpdates();
+if($update->has('message')) {
+    $message = $update->getMessage();
+    if($message->has('text')) {
+        switch($text = $message->getText()) {
+            case '/about':
+                $telegram->sendMessage([
+                    'chat_id' => $message->getChat()->getId(),
+                    'Sobre alguma coisa'
+                ]);
+                break;
+            case '/start':
+            case (preg_match('/^\/start (?<token>[a-f0-9]{32})/', $text, $matches) ? true : false):
+                break;
+        }
+    }
+}
